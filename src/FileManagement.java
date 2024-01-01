54afafbdc50d2d7713f62a3e166f337b98abfdd2
Yogimura <Vladimir.decastrod@gmail.com>
2024-01-01 15:24:09 -0400

    File implementation

--- src/FileManagement.java
+++ src/FileManagement.java
@@ -1,3 +1,23 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 public class FileManagement {
+    String name;
+    int length;
+    File content;
+    FileWriter replacer;
+    Scanner getter;
+    String URL;
+
+    public FileManagement(String nombre){
+        this.URL = gURL(nombre);
+        this.content = new File(this.URL);
+    }
+
+    public String gURL(String nombre){
+        return "\\Fileproves\\" + nombre + ".txt";
+    }
 
 }

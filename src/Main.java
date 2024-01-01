f0865c5fa855a379597739425c8d4869079d31a1
Yogimura <Vladimir.decastrod@gmail.com>
2024-01-01 14:20:14 -0400

    Nuevas funciones:
    getRepsWords
    wodsreplacement

--- src/Main.java
+++ src/Main.java
@@ -2,55 +2,67 @@ import java.util.*;
 
 public class Main {
     //static Scanner lector = new Scanner(System.in);
-    //static String[] Caracteres = new String[] {"!", "@", "#", "$", "%", "^", "&", "*", "(", ")"};
 
     public static ArrayList<String> getRepsWords(String[] base){
-        String comodin;
-        ArrayList<String> repetidas = new ArrayList<>();
+        String card;
+        ArrayList<String> repwords = new ArrayList<>();
 
         for(int i = 0; i < base.length; i++){
-            comodin = base[i];
+            card = base[i];
             for(int o = i+1; o < base.length; o++){
-                if(base[o].equals(comodin) && !repetidas.contains(comodin)){
-                    repetidas.add(comodin);
+                if(base[o].equals(card) && !repwords.contains(card)){
+                    repwords.add(card);
                     break;
                 }
             }
         }
 
-        return repetidas;
+        return repwords;
     }
-    public static void main(String[] args) {
-        String texto;
-        String resultado;
 
-        String[] palabras;
+    public static String[] wordsreplacement(String[] words, ArrayList<String> reps){
 
-        LinkedList<String> repspile;
+        String[] chars = new String[] {"!", "@", "#", "$", "%", "^", "&", "*", "(", ")"};
+        String card = "";
+        LinkedList<String> repspile = new LinkedList<>(reps);
 
-        ArrayList<String> repswords;
-        Map<Integer, String> coordenadas = new HashMap<>();
-
-
-        texto = "Pedro Almanzar del orbe porta un orbe cuyo propietario era el abuelo de su abuelo Almanzar del orbe";
-
-        System.out.println(texto);
-
-        resultado = texto;
-        palabras = texto.split(" ");
-
-        repswords = new ArrayList<>(getRepsWords(palabras));
-
-        repspile = new LinkedList<>(repswords);
-
-        for(int i = 0; i < palabras.length; i++) {
-            for(String repetida: repswords){
-                if(palabras[i].equals(repetida)){
-                    coordenadas.put(i, repetida);
+        for(int i = 0; i < reps.size(); i++){
+            card = reps.get(i);
+            for(int o = 0; o < words.length; o++){
+                if(words[o].equals(card)){
+                    if(repspile.contains(card)){
+                        repspile.poll();
+                    }else{
+                        words[o] = chars[i];
+                    }
                 }
             }
         }
 
-        System.out.println(resultado);
+        return words;
+    }
+
+    public static void main(String[] args) {
+        String text;
+        String result;
+        String[] words;
+        ArrayList<String> repswords;
+
+
+        text = "Pedro Almanzar del orbe porta un orbe cuyo propietario era el abuelo de su abuelo Almanzar del orbe";
+
+        System.out.println(text);
+
+        result = text;
+        words = text.split(" ");
+
+        repswords = new ArrayList<>(getRepsWords(words));
+        words = wordsreplacement(words, repswords);
+
+        assert words != null : "No hay entrada";
+        result = String.join(" ", words);
+
+
+        System.out.println(result);
     }
 }

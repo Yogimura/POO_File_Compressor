8ff0ed4c6059c16738c651f8b6c21ecf22ee1f24
Yogimura <Vladimir.decastrod@gmail.com>
2024-02-22 16:44:24 -0500

    Lambda expressions practice

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.nio.file.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     static Scanner lector = new Scanner(System.in);
@@ -10,19 +15,41 @@ public class Main {
         System.out.println("Todos los archivos que se encuentren dentro de esta carpeta se comprimiran o de descomprimiran segun los archivos esten compresos o no.");
         System.out.println("Quiere usted proceder?\n1. Si\n2. No\n0. Salir");
         int sl = -1;
-        int i = 0;
-        do {
-            if(i > 0){
-                System.out.println("Ingrese una opcion valida");
-            }
-            try {
-                sl = lector.nextInt();
-            } catch (InputMismatchException _) {
-                lector.nextLine();
-            }
-            i++;
-        } while (sl <= -1 || sl >= 3);
+        sl = verifyInput(sl, 0, 2);
+        ArrayList<AFile> Files = new ArrayList<>();
 
+        getFoldersFiles(Paths.get("Fileproves")).forEach(File -> {
+            if(File.endsWith(".ziped")){
+                try{
+                    Files.add(new Zipped(File));
+                }catch (FileNotFoundException ex){
+                    System.out.println("Archivo no se encuentra");
+                }
+            }else{
+                try{
+                    Files.add(new Unzipped(File));
+                }catch (FileNotFoundException ex){
+                    System.out.println("Archivo no se encuentra");
+                }
+            }
+        });
+
+        ArrayList<AFile> gogo = new ArrayList<>(Files.stream().filter(f -> f instanceof Unzipped).toList());
+        Files.removeIf(f -> f instanceof Unzipped);
+        Files.addAll(gogo);
+        Files.forEach(f -> System.out.println(f.carry + ":" + f.getClass()));
+    }
+
+    public static ArrayList<String> getFoldersFiles(Path path) throws IOException{
+        ArrayList<String> files = new ArrayList<>();
+
+        try(Stream<Path> filepath = Files.walk(path)){
+            filepath.forEach(Path -> files.add(Path.toString()));
+        }catch (IOException ex){
+            System.out.println("No existe");
+        }
+        files.remove(0);
+        return files;
     }
 
     public static void cleanScreen(long milis) throws InterruptedException{
@@ -31,5 +58,19 @@ public class Main {
         System.out.flush();
     }
 
-
+    public static int verifyInput(int set, int rangeA, int rangeB){
+        int i = 0;
+        do {
+            if(i > 0){
+                System.out.println("Ingrese una opcion valida");
+            }
+            try {
+                set = lector.nextInt();
+            } catch (InputMismatchException _) {
+                lector.nextLine();
+            }
+            i++;
+        } while (set < rangeA || set > rangeB);
+        return set;
+    }
 }

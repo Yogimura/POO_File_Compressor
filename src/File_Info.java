26d4403b30a2b23d394cf3e934df805c4ea53a5e
Yogimura <Vladimir.decastrod@gmail.com>
2024-02-25 14:48:30 -0500

    File_Info Documentation

--- src/File_Info.java
+++ src/File_Info.java
@@ -1,3 +1,9 @@
 import java.io.File;
+/**
+ * This record class is the ID of each {@link AFile} object.
+ * @param file is an instance of the {@link File} class that represents the real file.
+ * @param name is the name of the file, including his extension.
+ * @param path is the path of the file, the folder where it will be created or already is.
+ * */
 public record File_Info(File file, String name, String path) {
 }

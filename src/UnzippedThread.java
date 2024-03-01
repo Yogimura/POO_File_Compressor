9430a5d34f8509e6eb417f2ac09bf4e877343b63
Yogimura <Vladimir.decastrod@gmail.com>
2024-03-01 06:44:13 -0500

    UnzippedThread.java Documentation

--- src/UnzippedThread.java
+++ src/UnzippedThread.java
@@ -1,13 +1,32 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 //TODO in scope documentation
-//TODO general documentation
+/**
+ * This class extends {@link Thread}, This Zip all the {@link Unzipped} Files.
+ * */
 public class UnzippedThread extends Thread{
+    /**
+     * The {@link ArrayList} of {@link Unzipped} files.
+     * */
     ArrayList<Unzipped> files;
+
+    /**
+     * The {@link ArrayList} where the new {@link Zipped} files will be.
+     * */
     static ArrayList<Zipped> IOfiles = new ArrayList<>();
+
+    /**
+     * This constructor initialize {@link UnzippedThread#files}
+     * @param set the {@link ArrayList} of {@link Unzipped} files.
+     * */
     public UnzippedThread(ArrayList<Unzipped> set){
         this.files = new ArrayList<>(set);
     }
+
+    /**
+     * This override of {@link Thread#run()} use {@link Unzipped#IOzip(File_Info, HashMap)} to Zip all the files.
+     * */
     @Override
     public void run() {
         for(Unzipped go: files){
